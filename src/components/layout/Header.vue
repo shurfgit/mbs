4f5091c460a87269b1dff6c8baae356dc732870e
Denis Shuflyak <s-dshuflyak@aras.com>
2020-04-23 13:16:13 +0300

    Changed prettier/ESLint styles and corrected files accordingly Added Header.vue file

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="header">
+		<div id="main-menu"></div>
+		<div id="user-menu"></div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Header'
+};
+</script>
+
+<style scoped></style>
