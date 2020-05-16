d91e3f2a6355a4215af104a9f4cf7f6819d37b74
romanfilov <romanfilov96@gmail.com>
2020-05-16 18:33:22 +0300

    Add logic to check selected categories and save categories into .json file

--- src/components/structure/CategoriesSelection.vue
+++ src/components/structure/CategoriesSelection.vue
@@ -1,15 +1,17 @@
 <template>
     <li class="category-selection-item">
-        <input type="checkbox" ref="stateElem" class="category-select" v-model="localState">
-        <span>{{ category.name }}</span>
-        <ul class="category-internal-categories" v-if="category.children && category.children.length">
+        <label :for="category.id" class="checkbox-label">
+            <input :id="category.id" type="checkbox" ref="stateElem" class="category-select" v-model="localState">
+            <span>{{ category.name }}</span>
+        </label>
+        <ul class="category-internal-categories" v-if="hasChildren">
             <CategoriesSelection
                 v-for="c in category.children"
                 :key="c.id" 
-                :category="getCategoryById(c)"
-                :getCategoryById="getCategoryById"
+                :category="$store.getters.getCategoryById(c)"
                 v-model="state"
                 @status-changed="stateChanged($event)"
+                ref="childrenCategories"
             >
             </CategoriesSelection>
         </ul>
@@ -20,22 +22,29 @@
 	export default {
         name: "CategoriesSelection",
         data: () => ({
-            state: false
+            state: false,
+            isOpened: false
         }),
         methods: {
             stateChanged() {
-                if (this.$children.every((c) => c.localState && !c.$refs.stateElem.indeterminate)) {
+                if (this.$children.every((c) => c.state && !c.$refs.stateElem.indeterminate)) {
                     this.$refs.stateElem.indeterminate = false;
-                    this.localState = true;
-                } else if (this.$children.some((c) => c.localState || c.$refs.stateElem.indeterminate)) {
+                    this.state = true;
+                } else if (this.$children.some((c) => c.state || c.$refs.stateElem.indeterminate)) {
                     this.$refs.stateElem.indeterminate = true;
-                } else if (this.$children.every((c) => !c.localState || !c.$refs.stateElem.indeterminate)) {
+                } else if (this.$children.every((c) => !c.state || !c.$refs.stateElem.indeterminate)) {
                     this.$refs.stateElem.indeterminate = false;
-                    this.localState = false;
+                    this.state = false;
                 }
                 if (this.category.parent) {
                     this.$emit('status-changed');
                 }
+            },
+            getSelectedCategories() {
+                return this.$refs.stateElem.checked || this.$refs.stateElem.indeterminate ?
+                    [this.category].concat((this.$refs.childrenCategories || []).map(cat =>
+                        cat.getSelectedCategories()
+                    ).flat()) : [];
             }
         },
         computed: {
@@ -47,16 +56,19 @@
                     this.state = value;
                     this.$emit('status-changed');
                 }
+            },
+            hasChildren() {
+                return this.category.children && this.category.children.length;
             }
         },
         watch: {
             value(newState) {
+                this.$refs.stateElem.indeterminate = false;
                 this.state = newState;
             }
         },
         props: {
             category: Object,
-            getCategoryById: Function,
             value: Boolean
         }
 	}
@@ -65,4 +77,10 @@
 <style scoped lang="sass">
 .category-selection-item > .category-internal-categories
     margin-left: 20px
+
+.category-expander
+    cursor: pointer
+    margin-right: 10px
+    font-size: 16px
+    color: #ba4613
 </style>
--- src/pages/ExportImport.vue
+++ src/pages/ExportImport.vue
@@ -5,10 +5,10 @@
             <div class="categories">
                 <ul class="categories-selection-list">
                     <CategoriesSelection
-                        v-for="category in categories.filter(item => !item.parent)"
+                        v-for="category in $store.state.categories.filter(item => !item.parent)"
                         :key="category.id" 
                         :category="category"
-                        :getCategoryById="getCategoryById"
+                        ref="categories"
                     >
                     </CategoriesSelection>
                 </ul>
@@ -25,55 +25,17 @@
 		name: "ExportImport",
         components: {Toolbar, CategoriesSelection},
         methods: {
-            getCategoryById(id) {
-                return this.categories.find(item => item.id === id);
-            },
             exportCategories() {
-                console.log(this.$children['CategoriesSelection']);
+                let selectedCategories = this.$refs.categories.map(category => category.getSelectedCategories()).flat();
+                let data = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify({categories: selectedCategories}));
+                let linkElem = document.createElement('a');
+                linkElem.setAttribute('href', data);
+                linkElem.setAttribute('download', "categories.json");
+                linkElem.click();
             }
         },
         computed: {
-			categories() {
-				return [
-                    {
-                        id: 1,
-                        name: 'root',
-                        parent: null,
-                        children: [2, 4]
-                    },
-                    {
-                        id: 2,
-                        name: 'cat2',
-                        parent: 1,
-                        children: [5, 6]
-                    },
-                    {
-                        id: 3,
-                        name: 'cat3',
-                    },
-                    {
-                        id: 4,
-                        name: 'cat4',
-                        parent: 1,
-                        children: [7]
-                    },
-                    {
-                        id: 5,
-                        name: 'cat5',
-                        parent: 2
-                    },
-                    {
-                        id: 6,
-                        name: 'cat6',
-                        parent: 2
-                    },
-                    {
-                        id: 7,
-                        name: 'cat7',
-                        parent: 4
-                    }
-                ]
-            }
+
         }
 	}
 </script>
